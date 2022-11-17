8f1324058226f441fe6a739103c0ac372f5eee50
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-11-17 18:09:31 +0530

    ADDED: test cases for calculate fare

--- src/com/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/com/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -2,4 +2,9 @@ package com.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
 
+	public Object calculateFare(float distanceInKiloMeters, float timeInMinutes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
---
+++ src/com/cabinvoicegenerator/test/TestCalculateFare.java
@@ -0,0 +1,49 @@
+package com.cabinvoicegenerator.test;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import com.cabinvoicegenerator.CabInvoiceGenerator;
+
+@RunWith(Parameterized.class)
+public class TestCalculateFare {
+	private float distanceInKiloMeters, timeInMinutes, expectedFare;
+	private CabInvoiceGenerator cabInvoiceGenerator;
+	@Before
+	public void setUp() throws Exception {
+		cabInvoiceGenerator = new CabInvoiceGenerator();
+	}
+	
+	public TestCalculateFare(float distanceInKiloMeters, float timeInMinutes, float expectedFare) {
+		this.distanceInKiloMeters = distanceInKiloMeters;
+		this.timeInMinutes = timeInMinutes;
+		this.expectedFare = expectedFare;
+	}
+	
+	@Parameterized.Parameters
+	public static Collection testEmails() {
+		return Arrays.asList(new Object[][] {
+			{15, 54, 204},
+			{0.21f, 0.2f, 5},
+			{0.5f, 0.2f, 5.2f},
+			{0.1f, 3.4f, 5},
+			{0.1f, 5, 5.1f},
+			{0.1f, 5, 5.1f},
+			{0, 10, 10},
+			{10, 0, 100},
+			{1, 1, 11},
+		});
+	}
+	
+	@Test
+	public void test() {
+		Assert.assertEquals(expectedFare, cabInvoiceGenerator.calculateFare(distanceInKiloMeters, timeInMinutes));
+	}
+
+}
